f3547657633b07bbfd05ed32e46ec1784983b576
Tim001001001 <31822408+Tim001001001@users.noreply.github.com>
2017-10-18 17:27:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-var addNewTask = function() {
-    var newTask = $('input').val();
-    $('.item_list').append('<div class="item">' + newTask + '</div>');
-    $('input').val("");
-}
+$(document).ready(function () {
+
+    $('button').click(function () {
+        var newTask = $('input').val();
+        $('.item_list').append('<div class="item">' + newTask + '</div>');
+        $('input').val("");
+    });
+
+
+
+
+
+    $('input').keypress(function (e) {
+        if (event.which == 13) {
+            var newTask = $('input').val();
+            $('.item_list').append('<div class="item">' + newTask + '</div>');
+            $('input').val("");
+        }
+    });
+
+
+
+
+    $(".item").dblclick(function () {
+        $(this).toggleClass("rTasked");
+    });
+});
